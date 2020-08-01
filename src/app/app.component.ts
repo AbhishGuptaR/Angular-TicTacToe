fe8f052d5d4dc4f476af53e9a1f24f0517ffe9e1
Abhishek Gupta <abhishekguptaa3@gmail.com>
2020-08-01 18:20:20 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,120 @@
+import { Component } from '@angular/core';
+
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'tic-tac-toe';
+
+  winMessage: string = '';
+  isCross = false;
+  itemArray: string[] = new Array(9).fill('empty');
+
+  constructor(private toastr: ToastrService) {}
+
+  handleClick = (itemNumber: number) => {
+    if (this.winMessage) {
+      return this.toastr.success(this.winMessage);
+    }
+
+    if (this.itemArray[itemNumber] === 'empty') {
+      this.itemArray[itemNumber] = this.isCross ? 'cross' : 'circle';
+      this.isCross = !this.isCross;
+    } else {
+      return this.toastr.warning('Aldready Filled');
+    }
+
+    this.checkIsWinner();
+    this.checkDraw();
+  };
+
+  reloadGame = () => {
+    this.winMessage = '';
+    this.isCross = false;
+    this.itemArray = new Array(9).fill('empty');
+  };
+
+  checkDraw = () => {
+    let count = 0;
+    for (let i of this.itemArray) {
+      if (i !== 'empty' && this.winMessage === '') {
+        count++;
+      }
+    }
+    if (count === this.itemArray.length) {
+      this.winMessage = 'Draw';
+      return this.toastr.info('Game Drawn');
+    }
+  };
+
+  checkIsWinner = () => {
+    //first row
+    if (
+      this.itemArray[0] === this.itemArray[1] &&
+      this.itemArray[0] === this.itemArray[2] &&
+      this.itemArray[0] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[0]} Won !`;
+    }
+    //second row
+    if (
+      this.itemArray[3] === this.itemArray[4] &&
+      this.itemArray[3] === this.itemArray[5] &&
+      this.itemArray[3] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[3]} Won !`;
+    }
+    //third row
+    if (
+      this.itemArray[6] === this.itemArray[7] &&
+      this.itemArray[6] === this.itemArray[8] &&
+      this.itemArray[6] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[6]} Won !`;
+    }
+    //first col
+    if (
+      this.itemArray[0] === this.itemArray[3] &&
+      this.itemArray[0] === this.itemArray[6] &&
+      this.itemArray[0] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[0]} Won !`;
+    }
+    //second col
+    if (
+      this.itemArray[1] === this.itemArray[4] &&
+      this.itemArray[1] === this.itemArray[7] &&
+      this.itemArray[1] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[1]} Won !`;
+    }
+    //third col
+    if (
+      this.itemArray[2] === this.itemArray[5] &&
+      this.itemArray[2] === this.itemArray[8] &&
+      this.itemArray[2] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[2]} Won !`;
+    }
+    //diag top - bottom
+    if (
+      this.itemArray[0] === this.itemArray[4] &&
+      this.itemArray[0] === this.itemArray[8] &&
+      this.itemArray[0] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[0]} Won !`;
+    }
+    //diag bottom - top
+    if (
+      this.itemArray[2] === this.itemArray[4] &&
+      this.itemArray[2] === this.itemArray[6] &&
+      this.itemArray[2] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[2]} Won !`;
+    }
+  };
+}
